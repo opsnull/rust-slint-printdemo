d30d45abc02f1df6408d27db57f2a44adbcf65b0
opsnull <geekard@qq.com>
2024-05-14 11:29:50 +0800

    cleanup

--- mcu-board-support/lib.rs
+++ mcu-board-support/lib.rs
@@ -4,19 +4,19 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 extern crate alloc;
 
-#[cfg(feature = "esp32-s3-box")]
-mod esp32_s3_box;
-#[cfg(feature = "esp32-s3-box")]
+#[cfg(feature = "esp32-s3-box-3")]
+mod esp32_s3_box_3;
+#[cfg(feature = "esp32-s3-box-3")]
 pub mod gt911;
-#[cfg(feature = "esp32-s3-box")]
-pub use esp32_s3_box::*;
+#[cfg(feature = "esp32-s3-box-3")]
+pub use esp32_s3_box_3::*;
 
 #[cfg(not(any(
-    feature = "esp32-s3-box"
+    feature = "esp32-s3-box-3"
 )))]
 pub use i_slint_core_macros::identity as entry;
 
 #[cfg(not(any(
-    feature = "esp32-s3-box"
+    feature = "esp32-s3-box-3"
 )))]
 pub fn init() {}
